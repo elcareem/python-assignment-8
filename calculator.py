331fa4f7b1bf11029350e165546acd8067cd3797
your_name <andrewmiracle100@gmail.com>
2025-09-17 19:01:50 +0100

    complete calculator task

--- calculator.py
+++ calculator.py
@@ -12,3 +12,25 @@ Example Usage:
     print(calc.add(5, 3))        # 8
     print(calc.divide(10, 0))    # "Error: Division by zero"
 """
+
+class Calculator:
+
+    def add(self, a, b):
+        return a + b 
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+    
+    def divide(self, a, b):
+        if b == 0:
+            return "Error: Division by zero"
+        return a / b
+
+calc = Calculator()
+
+print(calc.add(5, 3))       
+print(calc.divide(10, 0))  
+
