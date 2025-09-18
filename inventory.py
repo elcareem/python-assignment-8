c5559539ed5fa4ee638f3d6ae30a7f88c829ad9b
your_name <andrewmiracle100@gmail.com>
2025-09-18 09:30:32 +0100

    complete inventory task

--- inventory.py
+++ inventory.py
@@ -22,3 +22,48 @@ Example Usage:
     favour_store.remove_item("Milk", 40)
     print(treasure_store.show_inventory())  # {"Milk": 10, "Garri": 30}
 """
+class Inventory:
+    def __init__(self):
+        self.store = {}
+
+    def add_item(self, item, quantity):
+        if item not in self.store:
+            if quantity > 0:
+                self.store.update({item: quantity})
+            else:
+                return "Invalid quantity"
+
+    def remove_item(self, item, quantity):
+        if item not in self.store:
+            return "Item not available"
+        if 0 < quantity <= self.store[item]: 
+            self.store.update({item: self.store[item] - quantity})        
+            if self.store[item] == 0:
+                self.store.pop(item)
+        else:
+            return "Invalid quantity"
+    
+    def update_stock(self, item, quantity):
+        if item in self.store:
+            if quantity > 0:
+                self.store.update({item: self.store[item] + quantity})
+            else:
+                return "Invalid quantity"
+
+
+    def show_inventory(self):
+        return self.store
+
+
+treasure_store = Inventory()
+favour_store = Inventory()
+
+treasure_store.add_item("Apples", 50)
+treasure_store.add_item("Bananas", 30)
+print(treasure_store.remove_item("Apples", 50))
+print(treasure_store.show_inventory())  
+
+favour_store.add_item("Milk", 50)
+favour_store.add_item("Garri", 30)
+favour_store.remove_item("Milk", 40)
+print(favour_store.show_inventory())  
