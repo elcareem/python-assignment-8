cfa7abe85130521b4fd89e221f6bc2d8aa049901
your_name <andrewmiracle100@gmail.com>
2025-09-19 08:27:26 +0100

    complete restaurant task

--- restaurant.py
+++ restaurant.py
@@ -15,3 +15,58 @@ Example Usage:
     order.add_item("Drink", 3)
     print(order.calculate_total())  # 5500
 """
+
+class RestaurantOrder:
+    def __init__(self, menu):
+        self.menu = menu
+        self.orders = {}
+
+    def add_item(self, item, quantity):
+        if item in self.menu:
+            if quantity > 0:
+                if item in self.orders:
+                    self.orders.update({item: self.orders[item] + quantity})
+                else:
+                    self.orders.update({item: quantity})
+            else:
+                print("Invalid quantity")
+        else:
+            print(f"{item} not available on menu")
+    
+
+    def remove_item(self, item, quantity):
+        if item in self.orders:
+            if 0 < quantity <= self.orders[item]:
+                self.orders.update({item: self.orders[item] - quantity})
+                if self.orders[item] == 0:
+                    self.orders.pop(item)
+            else:
+                print("Invalid quantity")
+        else:
+            print(f"{item} not in order")
+
+    
+    def calculate_total(self):
+        total = 0
+    
+        for order_item in self.orders:
+            for item, price in self.menu.items():
+                if order_item == item:
+                    total += self.orders[order_item] * price
+
+        return total
+            
+
+
+prices = {"Pizza": 2000, "Burger": 1500, "Drink": 500}
+order = RestaurantOrder(prices)
+order.add_item("Pizza", 2)
+order.add_item("Pizza", 2)
+order.add_item("Burger", 3)
+order.remove_item("Pizza", 2)
+
+print(order.menu)
+print(order.orders)
+
+print(order.calculate_total())
+
