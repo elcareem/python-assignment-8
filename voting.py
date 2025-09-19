540ce742046a5670736a52b9abb2d1c99391d559
your_name <andrewmiracle100@gmail.com>
2025-09-19 11:21:55 +0100

    complete voting task

--- voting.py
+++ voting.py
@@ -18,3 +18,55 @@ Example Usage:
     print(election.results())  # {"Alice": 2, "Bob": 1}
     print(election.winner()) # Alice
 """
+
+class VotingSystem:
+
+    def __init__(self):
+        self.candidates = []
+        self.voters = set()
+        self.results = {}
+
+    def register_candidate(self, candidate):
+        if candidate in self.candidates:
+            print("Candidate with name already exist")
+            return
+
+        self.candidates.append(candidate)
+        self.results.update({candidate: 0}) 
+
+    def vote(self, voter, candidate):
+        if voter not in self.voters:
+            if candidate in self.candidates:
+                self.voters.add(voter)
+                self.results.update({candidate: self.results[candidate] + 1})
+            else:
+                print("Candidate is not part of the election")
+
+        else:
+            print("Voter has voted already")
+
+    def result(self):
+        return self.results
+
+    def winner(self):
+        top_votes = 0
+        winner = []
+        for votes in self.results.values():
+            if votes > top_votes:
+                top_votes = votes
+            
+        for candidate in self.results:
+            if self.results[candidate] == top_votes:
+                winner.append(candidate)
+        return winner
+
+election = VotingSystem()
+election.register_candidate("Alice")
+election.register_candidate("Bob")
+election.vote("Voter1", "Alice")
+election.vote("Voter2", "Bob")
+election.vote("Voter3", "Alice")
+
+print(election.result())  # {"Alice": 2, "Bob": 1}
+print(election.winner())
+
